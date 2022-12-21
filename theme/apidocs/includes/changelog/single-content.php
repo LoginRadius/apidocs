9e3a0d30b2204d02be707b8805d6bcc402f93c92
INDRASEN <indrasenkumar715@gmail.com>
2022-12-21 12:07:51 +0530

    Update single-content.php

--- theme/apidocs/includes/changelog/single-content.php
+++ theme/apidocs/includes/changelog/single-content.php
@@ -48,9 +48,9 @@ $changelog['status'] = isset($changelogDoc->status) ? $changelogDoc->status : ''
     <div class="col-10">
         <div class="changelogsection">
             <?php
-                       echo "<pre>"; print_r($changelog);
+                   //    echo "<pre>"; print_r($changelog);
 
-            if (count($changelog['changelog']) > 0) {
+            if (true) {
                 ?><h1 style="font-size: 22pt; margin-top: 10px; margin-bottom: 10px;">Change Log</h1>
                 <table class="changelog">
                     <?php
